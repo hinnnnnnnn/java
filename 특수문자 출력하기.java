f5302b79638f0bfa9748f9b7c788ca15598b2162
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-26 22:20:13 +0900

    Create 특수문자 출력하기.java

---
+++ 특수문자 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+                         
+    }
+}
+
+/* 출력 결과
+  !@#$%^&*(\'"<>?:;
+*/
+
+🔑 기준: 특수문자를 "문자 그대로" 출력하기 위해 필요한 경우에만 역슬래시(\)를 붙임
