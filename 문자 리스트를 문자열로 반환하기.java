174e7fc9c6758ab369e930e89e006af74b72008e
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-10 21:37:53 +0900

    Create 문자 리스트를 문자열로 반환하기.java

---
+++ 문자 리스트를 문자열로 반환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        
+        for (String a : arr) { //⭐️for-each문⭐️
+              answer += a;
+        }
+
+        return answer;
+    }
+}
