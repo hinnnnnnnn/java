d7bbae6bc8dac398b4c6f8f710ec53bb528fff55
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-11 16:04:37 +0900

    Create n의 배수.java

---
+++ n의 배수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        if (num % n == 0) {
+            return 1; //Java에서는 int 리턴 메서드가 모든 경우에 return 값을 줘야 함
+        } else {
+            return answer;
+        }
+    }
+}
