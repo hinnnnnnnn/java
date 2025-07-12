1d38ac906ecd17dd0b556373a5a9f09776a560c9
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-12 23:04:02 +0900

    Update 코드 처리하기.java

--- 코드 처리하기.java
+++ 코드 처리하기.java
@@ -1,16 +1,15 @@
-
 class Solution {
     public String solution(String code) {
         StringBuilder answer = new StringBuilder();
-        int mode = 0;
+        int mode = 0; // 현재 모드를 나타냄 (0 또는 1)
         for (int i = 0; i < code.length(); i++) {
             char current = code.charAt(i);
-            if (current == '1') {
+            if (current == '1') {  // '1'을 만나면 mode를 전환하고 다음 문자로 넘어감
                 mode = mode == 0 ? 1 : 0;
                 continue;
             }
 
-            if (i % 2 == mode) {
+            if (i % 2 == mode) { // 현재 인덱스가 mode와 같은 홀/짝수일 때만 문자 추가
                 answer.append(current);
             }
         }
