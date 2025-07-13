0ffcf96d31e7fcd06bc04cadc46ccff60b4a6c98
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-13 23:38:31 +0900

    Update and rename ⭐️ 등차수열의 특정한 항만 더하기 to ⭐️ 등차수열의 특정한 항만 더하기.java

--- ⭐️ 등차수열의 특정한 항만 더하기.java
+++ ⭐️ 등차수열의 특정한 항만 더하기.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
 
-        for(int i = 0; i < included.length; i++){
+        for(int i = 0; i < included.length; i++){ //배열.length → 괄호 ❌
             if(included[i]){
-                answer +=  a + (d*i);
+                answer +=  a + (d*i); //등차수열의 i번째 항을 구하는 공식
             }
         }
 
