79aec21ebbb8f655be45a907c4ec808ec89bc750
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-14 23:04:22 +0900

    Update 원소들의 곱과 합.java

--- 원소들의 곱과 합.java
+++ 원소들의 곱과 합.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] num_list) {
-        int sum = 0;
-        int mul = 1;
+        int sum = 0; //sum: 숫자들을 모두 더할 변수 → 처음엔 0으로 시작
+        int mul = 1; //mul: 숫자들을 모두 곱할 변수 → 처음엔 1로 시작
         
         for(int num : num_list) {
             sum += num;
