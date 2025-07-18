9b644536f3d3782623f985106c624290023539d7
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-18 14:13:23 +0900

    Update 수열과 구간 쿼리3.java

--- 수열과 구간 쿼리3.java
+++ 수열과 구간 쿼리3.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for (int[] query : queries) {
-            int i = query[0], j = query[1];
+        for (int[] query : queries) { //queries 배열 안의 각 쌍(query)을 하나씩 꺼내 반복
+            int i = query[0], j = query[1]; //각 query에서 바꿀 인덱스 i와 j를 가져옴
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
-
         return arr;
     }
 }
