6dddb0cf5fb9734e246ba325d31d120daf9fbf87
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-23 17:39:02 +0900

    Update and rename d to 글자 이어 붙여 문자열 만들기.java

--- 글자 이어 붙여 문자열 만들기.java
+++ 글자 이어 붙여 문자열 만들기.java
@@ -2,11 +2,11 @@ class Solution {
     public String solution(String my_string, int[] index_list) {
         String answer = "";
         
-        for (int i=0; i<index_list.length; i++) {
-            int idx = index_list[i];
-            answer += my_string.charAt(idx);
-    
+        for (int i = 0; i < index_list.length; i++) {
+            int idx = index_list[i];          // 인덱스 번호를 꺼내고
+            answer += my_string.charAt(idx);  // 해당 위치 문자 가져와서 answer에 붙임
         }
+        
         return answer;
     }
 }
